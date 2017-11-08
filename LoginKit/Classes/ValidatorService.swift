4fa301795a9ba8e5b9780a99733ebe8d4e7e8296
Haowen You <hy9be@virginia.edu>
2017-11-07 21:14:29 -0500

    Further customize for a China mainland use

--- LoginKit/Classes/ValidatorService.swift
+++ LoginKit/Classes/ValidatorService.swift
@@ -10,12 +10,13 @@ import Foundation
 import Validator
 
 enum ValidationError: String, Error {
-
+    
     case invalidName = "Invalid name"
     case invalidEmail = "Invalid email address"
     case passwordLength = "Must be at least 8 characters"
     case passwordNotEqual = "Password does not match"
-
+    case nonBlank = "不能为空"
+    
     var message: String {
         return self.rawValue
     }
@@ -23,64 +24,71 @@ enum ValidationError: String, Error {
 }
 
 public struct FullNameRule: ValidationRule {
-
+    
     public typealias InputType = String
-
+    
     public var error: Error
-
+    
     public init(error: Error) {
         self.error = error
     }
-
+    
     public func validate(input: String?) -> Bool {
         /*guard let input = input else {
-            return false
-        }
-
-        let components = input.components(separatedBy: " ")
-
-        guard components.count > 1 else {
-            return false
-        }
-
-        guard let first = components.first, let last = components.last else {
-            return false
-        }
+         return false
+         }
+         
+         let components = input.components(separatedBy: " ")
+         
+         guard components.count > 1 else {
+         return false
+         }
+         
+         guard let first = components.first, let last = components.last else {
+         return false
+         }
+         
+         guard first.characters.count > 1, last.characters.count > 1 else {
+         return false
+         }*/
         
-        guard first.characters.count > 1, last.characters.count > 1 else {
-            return false
-        }*/
-
         return true
     }
-
+    
 }
 
 struct ValidationService {
-
+    
     static var emailRules: ValidationRuleSet<String> {
         var emailRules = ValidationRuleSet<String>()
         emailRules.add(rule: emailRule)
         return emailRules
     }
-
+    
     static var passwordRules: ValidationRuleSet<String> {
         var passwordRules = ValidationRuleSet<String>()
         passwordRules.add(rule: ValidationRuleLength(min: 8, error: ValidationError.passwordLength))
         return passwordRules
     }
-
+    
+    static var nonBlankRules: ValidationRuleSet<String> {
+        var nonBlankRules = ValidationRuleSet<String>()
+        nonBlankRules.add(rule: ValidationRuleLength(min: 1, error: ValidationError.nonBlank))
+        return nonBlankRules
+    }
+    
     static var nameRules: ValidationRuleSet<String> {
         var nameRules = ValidationRuleSet<String>()
         nameRules.add(rule: FullNameRule(error: ValidationError.invalidName))
         return nameRules
     }
-
+    
     // MARK: - Private
-
+    
     private static var emailRule: ValidationRulePattern {
         return ValidationRulePattern(pattern: EmailValidationPattern.standard,
                                      error: ValidationError.invalidEmail)
     }
-
+    
 }
+
